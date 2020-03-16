e430e46e123b3ffe2f7844ffd5d21bce99b8f3db
sakethkura <saketh.kura@gmail.com>
2020-03-15 22:12:17 -0400

    added gson to the file and added more functionality to app

--- app/src/main/java/com/kurasaketh/testfrag/Game.java
+++ app/src/main/java/com/kurasaketh/testfrag/Game.java
@@ -8,17 +8,21 @@ import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+
 
 public class Game extends Fragment {
     TextView timer;
     TextView name;
     Button button;
+    TextView score;
     SharedPreferences sharedPreferences;
     @Nullable
     @Override
@@ -27,13 +31,28 @@ public class Game extends Fragment {
         sharedPreferences = getActivity().getSharedPreferences("com.TestFrag.app", Context.MODE_PRIVATE);
 
         View v = inflater.inflate(R.layout.game, container, false);
+        score = v.findViewById(R.id.score);
         name = v.findViewById(R.id.name);
         name.setText(sharedPreferences.getString("name", "User"));
         button = v.findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if(!timer.getText().equals("Done!")) {
+                    int value = sharedPreferences.getInt("score", 0) + 1;
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putInt("score", value);
+                    editor.apply();
+                    score.setText(String.format("Score: %d", value));
+                }
+                else{
+                    Player p = new Player();
+                    p.setName(sharedPreferences.getString("name", "User"));
+                    p.setScore(sharedPreferences.getInt("score", 0));
+                    Gson g = new Gson();
+                    Log.i("gson", g.toJson(p));
 
+                }
             }
         });
         timer = v.findViewById(R.id.timer);
---
+++ app/src/main/java/com/kurasaketh/testfrag/Player.java
@@ -0,0 +1,22 @@
+package com.kurasaketh.testfrag;
+
+public class Player {
+    private int score;
+    private String name;
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
